cdadc26474f782ff898dc3a338a07ea3ed0c61f8
Haman Oumarou Ali <80493817+HamaniOumarouc@users.noreply.github.com>
2025-05-22 09:06:24 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Ce script vérifie si un fichier donné existe ou non
-read -p "Entrez le nom du fichier : " filename="sample.txt"
-if [ -f "$sample.txt" ]; then
-echo "Le fichier '$sample.txt' existe."
+read -p "Entrez le nom du fichier : " filename
+# Vérifie si le fichier existe en utilisant la condition -f
+if [ -f "$filename" ]; then
+  echo "Le fichier '$filename' existe."
 else
-echo "Le fichier '$sample.txt' n'existe pas."
+  echo "Le fichier '$filename' n'existe pas."
 fi
